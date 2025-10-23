ae86a019941356455a793756afa7d63e09276783
Mohammad Anisi <ameenanisi10@gmail.com>
2025-10-23 14:03:35 -0400

    register

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 
-// update with correct schema
-
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  displayName: String,
-  bio: String,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
 }, { timestamps: true });
 
 export const User = mongoose.model('User', userSchema);
