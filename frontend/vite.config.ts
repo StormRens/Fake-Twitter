5304e8ed38ef9d587ed4d78862870d97a4e2c96b
Storm Rens <stormrens@users.noreply.github.com>
2025-11-04 15:48:44 +0000

    Update frontend/backend packages as well as routing for server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [react(), reactRouter()],
+  server: { host: "0.0.0.0", port: 3000 },
+  preview: { host: "0.0.0.0", port: 3000 }
 });
