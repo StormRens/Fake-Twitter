b01c863c976bf3ff867d4e0fc501aed527b63bce
Matheus <mathgoes@hotmail.com>
2025-11-13 14:29:01 -0500

    update routes

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,4 +1,15 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, layout } from "@react-router/dev/routes";
 
 
-export default [index("./routes/home.tsx"), route("/login", "./routes/login.tsx")] satisfies RouteConfig;
+export default [
+
+    // Public home page
+    index("./routes/home.tsx"),
+
+    // All AUTH pages share AuthLayout
+    layout("./layout/AuthLayout.tsx", [
+        route("/login", "./routes/login.tsx"),
+        route("/register", "./routes/register.tsx")
+    ]),
+    
+] satisfies RouteConfig;
