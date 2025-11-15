8f3161cd927e45ee817568b65a700cef975d1686
Matheus <mathgoes@hotmail.com>
2025-11-14 23:01:00 -0500

    important fix on vite.conf.ts , react was used twice

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig({
-  plugins: [react(), reactRouter(), tailwindcss()],
+  plugins: [reactRouter(), tailwindcss()],
   server: { host: "0.0.0.0", port: 3000 },
   preview: { host: "0.0.0.0", port: 3000 }
 });
